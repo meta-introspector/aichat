563faada7df266ba84f5aa81a37100dc0142b160
mike dupont <h4@solfunmeme.com>
2025-09-01 09:00:43 -0400

    build passing

--- src/auth/oauth_split/oauth_client_setup.rs
+++ src/auth/oauth_split/oauth_client_setup.rs
@@ -2,15 +2,16 @@ use anyhow::Result;
 use oauth2::{AuthUrl, ClientId, ClientSecret, TokenUrl, PkceCodeChallenge, PkceCodeVerifier};
 
 use crate::auth::oauth_split::constants::GoogleOAuthClient;
+use oauth2::basic::BasicClient;
 
 pub fn setup_oauth_client(
     client_id: String,
     client_secret: String,
 ) -> Result<(GoogleOAuthClient, PkceCodeChallenge, PkceCodeVerifier)> {
-    let client = GoogleOAuthClient::new(
+    let client = BasicClient::new(
         ClientId::new(client_id),
-        Some(ClientSecret::new(client_secret)),
     )
+    .set_client_secret(ClientSecret::new(client_secret))
     .set_auth_uri(AuthUrl::new("https://accounts.google.com/o/oauth2/v2/auth".to_string())?)
     .set_token_uri(TokenUrl::new("https://oauth2.googleapis.com/token".to_string())?);
 
